d73cbf69724a3f7e627d21f7384c7360d8df6899
Jacksheng127 <limjacksheng127@gmail.com>
2025-05-04 16:10:20 +0800

    convert to env

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,9 @@
 import { Platform } from 'react-native';
+import { DEFAULT_API_URL } from '@env';
 
 // Default fallback URL if env variable is not set
 // const DEFAULT_API_URL = 'http://10.192.49.72:8000';
-const DEFAULT_API_URL = 'https://e386-2001-d08-2181-6ce3-8bc1-d41c-2d33-1f65.ngrok-free.app';
+// const DEFAULT_API_URL = 'https://e386-2001-d08-2181-6ce3-8bc1-d41c-2d33-1f65.ngrok-free.app';
 
 // Get the API URL from environment variables
 export const API_URL = DEFAULT_API_URL ;
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,13 +1,13 @@
 // firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { GOOGLE_API_KEY } from "@env";
+import { GOOGLE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_APP_ID } from "@env";
 
 const firebaseConfig = {
   apiKey: GOOGLE_API_KEY,
-  authDomain: "medimate.firebaseapp.com",
-  projectId: "medimate-adfdf",
-  appId: "1:336819940127:android:8a65ac169ec550e38bde2a",
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  appId: FIREBASE_APP_ID,
   // etc.
 };
 
